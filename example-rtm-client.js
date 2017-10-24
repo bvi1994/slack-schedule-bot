78f8ba0f6d7534f7c77aba6cb43bf0f72b812ac4
bvi1994 <bvi1@ucsc.edu>
2017-10-24 10:52:45 -0700

    Updated example-rtm to have similar structure as moose. Got handleRTMmessage to work

--- example-rtm-client.js
+++ example-rtm-client.js
@@ -4,22 +4,33 @@
 
 /* eslint no-console:0 */
 
-var RtmClient = require('@slack/client').RtmClient;
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var {WebClient, RtmClient, RTM_EVENTS} = require('@slack/client')
+// var RtmClient = require('@slack/client').RtmClient;
+// var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 var token = process.env.SLACK_API_TOKEN || '';
 
-var rtm = new RtmClient(token, { logLevel: 'debug' });
+var web = new WebClient(token);
+var rtm = new RtmClient(token);
+// var rtm = new RtmClient(token, { logLevel: 'debug' });
 rtm.start();
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  console.log('Message:', message);
+  // console.log('Message:', message);
+  if(!message.user){
+    // web.chat.postMessage(message.channel, `You said: ${message.text}`);
+    // console.log('Pong!');
+    console.log("Message sent by a bot. Ignoring.")
+    return;
+  }
+  web.chat.postMessage(message.channel,
+    `Hello. I'm your appointment bot. Please give me access to your Google calander at http://localhost:3000/setup?slackId=${message.user}`)
 });
 
-rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
-  console.log('Reaction added:', reaction);
-});
-
-rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
-  console.log('Reaction removed:', reaction);
-});
+// rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
+//   console.log('Reaction added:', reaction);
+// });
+//
+// rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
+//   console.log('Reaction removed:', reaction);
+// });
