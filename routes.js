7dfb03cef0b9ac7e917b06be77e9e627967b59c9
bvi1994 <bvi1@ucsc.edu>
2017-10-23 20:44:39 -0700

    first commit, got the command to work per example

---
+++ routes.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var app = express();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.post('/')
