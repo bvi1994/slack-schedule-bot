05b097fee2084f04359bf3c81eee5b65161ca150
Emma Paschke <epaschke>
2017-10-25 11:49:25 -0700

    fixed pathways

--- app.js
+++ app.js
@@ -15,20 +15,30 @@ app.post('/interactive', function(req, res) {
   var resp = JSON.parse(req.body.payload);
   var saveEvent = resp.actions[0].value === 'yes';
   var message;
+  var user;
   User.findOne({Name: resp.user.id})
-  .then(function(user){
+  .then(function(u){
+      user = u;
     if(saveEvent && !user.Google.isSetupComplete){
         message = `Hey. I'm a scheduler bot. I need permission to access your calendar application. Please give me permission to hack to your Google Calender. http://localhost:3000/setup?Name=${user.Name}`;
+        res.send(message);
+        throw ("need permissions");
+    }
+    if (!saveEvent){
+        message = "Reminder not added"
         return;
     }
-    message = saveEvent ? "Reminder added." : "Reminder not added";
+    message = "Reminder added.";
+    return google.createCalendarEvent(user.Google.tokens, user.Pending.Subject, user.Pending.Date)
+  })
+  .then(function(){
     user.Pending = null;
     return user.save();
   })
   .then(function(){
-    console.log('User saved.');
-    console.log('Message');
-    res.send(message);
+      console.log('User saved.');
+      console.log('Message');
+      res.send(message);
   })
   .catch(function(err){
     res.send('Error:' + err);
@@ -38,7 +48,6 @@ app.post('/interactive', function(req, res) {
 app.get('/setup', function(req, res){
   var url = google.generateAuthUrl(req.query.Name);
   res.redirect(url);
-  // console.log(url);
 });
 
 app.get('/google/callback', function(req, res){
@@ -57,7 +66,14 @@ app.get('/google/callback', function(req, res){
       return user.save();
     })
     .then(function(){
-      res.send('Google authentication successful.');
+        return google.createCalendarEvent(user.Google.tokens, user.Pending.Subject, user.Pending.Date)
+    })
+    .then(function(){
+      user.Pending = null;
+      return user.save();
+    })
+    .then(function(){
+      res.send('Google authentication successful. Created reminder.');
     })
     .catch(function(err){
       console.log('Error:',err);
