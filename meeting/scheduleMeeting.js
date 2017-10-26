9b5869f7225e488aa78277e62829d39a1d69aa98
Emma Paschke <epaschke>
2017-10-26 15:10:45 -0700

    meeting funcs

--- meeting/scheduleMeeting.js
+++ meeting/scheduleMeeting.js
@@ -1,10 +1,8 @@
-function scheduleMeeting(invitee){ //Receives pending.invitees
+var google = require('../google');
 
-
-
-
+function scheduleMeeting(invitee, pending) {
+    return google.createCalendarEvent(invitee.Google.tokens, pending)
 }
-
 
 module.exports = {
   scheduleMeeting
