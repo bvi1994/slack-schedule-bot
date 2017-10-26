13abf92f34025429d3661c17cf93f9388120be32
bvi1994 <bvi1@ucsc.edu>
2017-10-26 14:25:28 -0700

    Commit

--- checkConflicts.js
+++ checkConflicts.js
@@ -1,16 +1,7 @@
-function checkConflict(user, pending){
+function checkConflict(startTime, endTime){
 
 }
 
 module.exports = {
   checkConflict
 };
-
-// dateTime: `${pending['Date']}T${pending.Time}`
-
-// how I added an hour to the Time
-// var newTime = pending.Time ? pending.Time.split(':') : null;
-// var timeNum = parseInt(newTime[0]) + 1;
-// newTime[0] = timeNum >= 10 ? timeNum.toString() : `0${timeNum.toString()}`;
-// newTime = newTime.join(':');
-// NEWTIME becomes an hour later than the start time
