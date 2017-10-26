eaa77f7fa36e5b6e05ffb6546a0517f0e1bc72eb
bvi1994 <bvi1@ucsc.edu>
2017-10-26 15:57:18 -0700

    Starting conflict.js

--- checkConflicts.js
+++ checkConflicts.js
@@ -1,4 +1,36 @@
+var google = require('googleapis');
+
 function checkConflict(user, pending){
+  // var newTime = pending.Time ? pending.Time.split(':') : null;
+  var startTime = pending.Time ? pending.Time.split(':') : null;
+  // var timeNum = parseInt(newTime[0]) + 1; endTime
+  var endTime[0] = !("Duration" in pending) ? parseInt(startTime[0]) + 1 : [parseInt(startTime[0]) + Math.floor(pending.Duration)]
+  var addedMinutes = (pending.Duration - Math.floor(pending.Duration)) * 60
+  if(startTime[1] + addedMinutes > 60){
+    endTime[0] = (parseInt(startTime[0]) + 1) % 24;
+    endTime[1] = parseInt(startTime[1]) + addedMinutes - 60
+  }
+  endTime[0] = endTime[0] >= 10 ? endTime[0].toString() : `0${endTime[0].toString()}`;
+  endTime[1] = endTime[1] >= 10 ? endTime[1].toString() : `0${endTime[1].toString()}`;
+  endTime = endTime.join(':');
+
+  var dateTimeStart = `${pending['Date']}T${pending.Time}`
+  var dateTimeEnd = `${pending['Date']}T${endTime}`
+
+  var check = {
+    'timeMin': dateTimeStart,
+    'timeMax': dateTimeEnd,
+    'timeZone': 'America/Los_Angeles',
+    'items': [
+      {
+        'id': 'primary'
+      }
+    ]
+  }
+
+  var calendar = google.calendar('v3');
+  var response = calendar.Freebusy.query(check);
+  console.log(response.kind);
 
 }
 
@@ -9,8 +41,4 @@ module.exports = {
 // dateTime: `${pending['Date']}T${pending.Time}`
 
 // how I added an hour to the Time
-// var newTime = pending.Time ? pending.Time.split(':') : null;
-// var timeNum = parseInt(newTime[0]) + 1;
-// newTime[0] = timeNum >= 10 ? timeNum.toString() : `0${timeNum.toString()}`;
-// newTime = newTime.join(':');
 // NEWTIME becomes an hour later than the start time
