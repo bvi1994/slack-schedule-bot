58311d7c988130abfc0fe0bb71cc21ffd4c3f91e
crawforde <crawforde@kenyon.edu>
2017-10-26 13:48:05 -0700

    Fixed assync stuff in sendReminder.js

--- reminders/sendReminder.js
+++ reminders/sendReminder.js
@@ -4,29 +4,46 @@ var token = process.env.SLACK_API_TOKEN || '';
 var web = new WebClient(token);
 
 function sendReminder(reminder){
+  if(reminder.Channel === 'test'){
+    return sendTestReminder(reminder);
+  }
+  return sendRealReminder(reminder);
+};
+
+function sendTestReminder(reminder){
   var todaysDate = new Date();
   todaysDate.setHours(0,0,0,0);
+  var message = `Reminding ${reminder.Channel} to ${reminder.Subject} ${today ? 'today' : 'tomorrow'}`;
   var today = reminder.Date.getTime() === todaysDate.getTime();
-  var message;
-  var remove = true;
-  if(reminder.Channel === 'test'){
-    message = `Reminding ${reminder.Channel} to ${reminder.Subject} ${today ? 'today' : 'tomorrow'}`;
-  }
-  else{
-    message = `Reminder to ${reminder.Subject} ${today ? 'today' : 'tomorrow'}`;
-    try{
-      web.chat.postMessage(reminder.Channel, message);
-      console.log(message);
-    } catch(error){
-      remove = false;
-    }
-  }
-  if(today && remove){
+  if(today){
     console.log(`Reminder ${reminder._id} removed from database.`);
     return reminder.remove();
   }
   return new Promise(function(resolve, reject) {
     resolve();
+  });
+};
+
+function sendRealReminder(reminder){
+  var todaysDate = new Date();
+  todaysDate.setHours(0,0,0,0);
+  var today = reminder.Date.getTime() === todaysDate.getTime();
+  var message = `Reminder to ${reminder.Subject} ${today ? 'today' : 'tomorrow'}`;
+  web.chat.postMessage(reminder.Channel, message)
+  .then(function(){
+    console.log(message);
+    if(today){
+      console.log(`Reminder ${reminder._id} removed from database.`);
+      return reminder.remove();
+    }
+    return new Promise(function(resolve, reject) {
+      resolve();
+    });
+  })
+  .catch(function(err){
+    return new Promise(function(resolve, reject) {
+      resolve();
+    });
   });
 };
 
