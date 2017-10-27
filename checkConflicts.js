b8ad798b15a1985c85c81b6e83c0883e15b71f65
crawforde <crawforde@kenyon.edu>
2017-10-27 11:55:18 -0700

    Restructured code with Brandon's changes

--- checkConflicts.js
+++ checkConflicts.js
@@ -1,16 +1,61 @@
-function checkConflict(user, pending){
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
 
+function getAuthClient() {
+  return new OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    //'http://localhost:3000/google/callback'
+    process.env.DOMAIN + '/google/callback'
+  );
+}
+
+
+async function checkConflict(user, pending){
+  // var newTime = pending.Time ? pending.Time.split(':') : null;
+  var client = getAuthClient();
+  var tokens = user.Google.tokens;
+  client.setCredentials(tokens)
+  var startDate = pending.Time ? pending.Time.split(':') : null;
+  var startTime = new Date(pending.Date + 'T' + startDate.join(':'));
+  // console.log(pending.Duration);
+  var endTime = new Date(pending.Date + 'T' + startDate.join(':'));
+  var duration = 3600000 // TO_DO: Calculate millseconds for arbitary duration. See Google.js
+  endTime.setTime(endTime.getTime() + duration);
+  // console.log(startTime);
+  // console.log(endTime);
+
+
+  var check = {
+    'auth': client,
+    'resource': {
+      'timeMin': startTime,
+      'timeMax': endTime,
+      'timeZone': 'America/Los_Angeles',
+      'items': [
+        {
+          'id': 'primary',
+          'busy': 'active'
+        }
+      ]
+    }
+  }
+
+  var calendar = google.calendar('v3');
+  calendar.freebusy.query(check, function(error, schedule){
+    if(error){
+      console.log('Error: ', error);
+      return error
+    } else {
+      console.log('Schedule ', schedule.calendars.primary.busy)
+      if(schedule.calenders.primary.busy){
+        return false
+      }
+      return true
+    }
+  })
 }
 
 module.exports = {
   checkConflict
 };
-
-// dateTime: `${pending['Date']}T${pending.Time}`
-
-// how I added an hour to the Time
-// var newTime = pending.Time ? pending.Time.split(':') : null;
-// var timeNum = parseInt(newTime[0]) + 1;
-// newTime[0] = timeNum >= 10 ? timeNum.toString() : `0${timeNum.toString()}`;
-// newTime = newTime.join(':');
-// NEWTIME becomes an hour later than the start time
